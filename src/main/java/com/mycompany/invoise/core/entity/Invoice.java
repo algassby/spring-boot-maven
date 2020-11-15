5d23ca06182eb28a7ee9a62d65a5f0d5957ee163
algassby <algassby@hotmail.com>
2020-11-15 15:12:18 +0100

    utilisaton EntityGraphe pour effectuer la requete findAll avec la jointure

--- src/main/java/com/mycompany/invoise/core/entity/Invoice.java
+++ src/main/java/com/mycompany/invoise/core/entity/Invoice.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@NamedEntityGraph(name = "invoise.customer", attributeNodes= @NamedAttributeNode("customer"))
 public class Invoice {
 
     @Id
